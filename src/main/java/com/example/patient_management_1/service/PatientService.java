17173979dd1390d486ded9c5e1d55e9babd82ee9
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-04 20:35:03 +0530

    adding Doctor Service & getPatientById method init

---
+++ src/main/java/com/example/patient_management_1/service/PatientService.java
@@ -0,0 +1,26 @@
+package com.example.patient_management_1.service;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.patient_management_1.entity.Doctor;
+import com.example.patient_management_1.entity.Patient;
+import com.example.patient_management_1.repository.PatientRepository;
+
+@Service
+public class PatientService {
+    
+    @Autowired
+    PatientRepository patientRepository;
+
+    public Patient getPatientById(Long id){
+        Patient patient = patientRepository.findById(id).get();
+        if (patient == null) {
+            throw new NoSuchElementException("patient not found by id: " + id);
+        }
+
+        return patient;
+    }
+}
