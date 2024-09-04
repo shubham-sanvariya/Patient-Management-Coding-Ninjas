676b9167bb61d97221d3a53fe21ea362952dfa7e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-04 19:32:27 +0530

    adding Doctor entity

---
+++ src/main/java/com/example/patient_management_1/entity/Doctor.java
@@ -0,0 +1,40 @@
+package com.example.patient_management_1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    
+    private String specialty;
+
+    public Doctor() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSpecialty() {
+        return specialty;
+    }
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    
+}
