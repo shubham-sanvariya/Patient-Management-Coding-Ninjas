9168ba3b8b10c6a01e2fbc86a70f99be36de6cbb
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-04 21:51:29 +0530

    adding  method saveDoctorForPatient in DoctorController

--- src/main/java/com/example/patient_management_1/service/DoctorService.java
+++ src/main/java/com/example/patient_management_1/service/DoctorService.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.patient_management_1.entity.Doctor;
+import com.example.patient_management_1.entity.Patient;
 import com.example.patient_management_1.repository.DoctorRepository;
+import com.example.patient_management_1.repository.PatientRepository;
 
 @Service
 public class DoctorService {
     
     @Autowired
     DoctorRepository doctorRepository;
+
+    @Autowired
+    PatientRepository patientRepository;
 
     public Doctor getDoctorById(Long id){
         Doctor doctor = doctorRepository.findById(id).get();
@@ -23,12 +28,19 @@ public class DoctorService {
         return doctor;
     }
 
-    public void saveDoctor(Doctor doctor){
-        Doctor dr = doctorRepository.findById(doctor.getId()).get();
-        if (dr == null) {
-            throw new NoSuchElementException("doctor not found by id: " + doctor.getId());
+    public void saveDoctor(Long patientId,Doctor doctor){
+        Patient patient = patientRepository.findById(patientId)
+        .orElseThrow(() -> new NoSuchElementException("patient not found by id: " + patientId));
+
+        Doctor dr = doctorRepository.findById(doctor.getId()).orElse(null);
+
+        if (dr != null) {
+            throw new NoSuchElementException("doctor already exists by id: " + doctor.getId());
         }
+        
         doctorRepository.save(doctor);
+        patient.setDoctor(doctor);
+        patientRepository.save(patient);
     }
 
     public void updateDoctor(Doctor doctor){
