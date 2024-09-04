9cd86c9ef6d666d4dddbdbf99afb0d5ae74470b1
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-04 22:37:03 +0530

    adding AddressController with method getAddressById

---
+++ src/main/java/com/example/patient_management_1/controller/AddressController.java
@@ -0,0 +1,22 @@
+package com.example.patient_management_1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.patient_management_1.entity.Address;
+import com.example.patient_management_1.service.AddressService;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+    @Autowired
+    AddressService addressService;
+
+    @GetMapping("/{id}")
+    public Address getAddressById(@PathVariable Long id){
+        return addressService.getAddressById(id);
+    }
+}
