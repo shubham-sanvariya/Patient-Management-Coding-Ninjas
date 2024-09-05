aa1901bcc2e12b6ce50e077dd4a544cd9f8c709b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-05 10:23:37 +0530

    adding  savePatient in PatientController

--- src/main/java/com/example/patient_management_1/controller/PatientController.java
+++ src/main/java/com/example/patient_management_1/controller/PatientController.java
@@ -3,6 +3,8 @@ package com.example.patient_management_1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class PatientController {
     @GetMapping("/{id}")
     public Patient getPatientById(@PathVariable Long id){
         return patientService.getPatientById(id);
+    }
+
+    @PostMapping("/add")
+    public void savePatient(@RequestBody Patient patient){
+        patientService.savePatient(patient);
     }
 }
