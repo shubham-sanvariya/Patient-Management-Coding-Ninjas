b9f1c933767ada8525e8826335d005bef7a861b7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-05 10:18:59 +0530

    adding PatientController with getPatientById init

---
+++ src/main/java/com/example/patient_management_1/controller/PatientController.java
@@ -0,0 +1,23 @@
+package com.example.patient_management_1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.patient_management_1.entity.Patient;
+import com.example.patient_management_1.service.PatientService;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+    
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping("/{id}")
+    public Patient getPatientById(@PathVariable Long id){
+        return patientService.getPatientById(id);
+    }
+}
