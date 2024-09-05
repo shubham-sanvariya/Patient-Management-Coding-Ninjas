50d439afba254ca4880dee1cb2c0f131fa8a2ab0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-05 10:24:03 +0530

    adding  updatePatient in PatientController

--- src/main/java/com/example/patient_management_1/controller/PatientController.java
+++ src/main/java/com/example/patient_management_1/controller/PatientController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,17 +15,22 @@ import com.example.patient_management_1.service.PatientService;
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
-    
+
     @Autowired
     PatientService patientService;
 
     @GetMapping("/{id}")
-    public Patient getPatientById(@PathVariable Long id){
+    public Patient getPatientById(@PathVariable Long id) {
         return patientService.getPatientById(id);
     }
 
     @PostMapping("/add")
-    public void savePatient(@RequestBody Patient patient){
+    public void savePatient(@RequestBody Patient patient) {
         patientService.savePatient(patient);
+    }
+
+    @PutMapping("/update")
+    public void updatePatient(@RequestBody Patient patient) {
+        patientService.updatePatient(patient);
     }
 }
